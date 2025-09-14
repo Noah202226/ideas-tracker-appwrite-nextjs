ec62f3f14be10db401806699f3d41cd763dc9327
Noa <noaligpitan26@gmail.com>
2025-09-14 16:34:48 +0800

    initial setup

---
+++ hooks/useAuth.ts
@@ -0,0 +1,58 @@
+// hooks/useAuth.ts
+
+import { useState, useEffect } from "react";
+import { account } from "../lib/appwrite";
+import { ID } from "appwrite";
+import type { Models } from "appwrite";
+import { useRouter } from "next/navigation";
+
+export function useAuth() {
+  const [current, setCurrent] =
+    useState<Models.User<Models.Preferences> | null>(null);
+
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  const register = async (email: string, password: string): Promise<void> => {
+    await account.create(ID.unique(), email, password);
+    await login(email, password);
+  };
+
+  const login = async (email: string, password: string): Promise<void> => {
+    const session = await account.createEmailPasswordSession(email, password);
+    console.log("session", session);
+    const user = await account.get();
+    setCurrent(user);
+    router.push("/");
+  };
+
+  const logout = async (): Promise<void> => {
+    await account.deleteSession("current");
+    setCurrent(null);
+    router.push("/");
+  };
+
+  const getCurrentUser = async () => {
+    try {
+      const user = await account.get();
+      setCurrent(user);
+    } catch (error) {
+      console.log("No current user", error);
+      setCurrent(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  return {
+    current,
+    loading,
+    login,
+    logout,
+    register,
+  };
+}
